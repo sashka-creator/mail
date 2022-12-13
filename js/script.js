5ce78505197c6f4a892118d5e2670263d587bbd8
Sasha <bigoleksandr@gmail.com>
2022-12-13 16:57:57 +0200

    +

--- js/script.js
+++ js/script.js
@@ -1,40 +1,69 @@
 let first = document.querySelectorAll('.first');
 for (let i = 0; i < first.length; i++){
-    window.addEventListener('mousemove', function(e) { 
+    window.addEventListener('mousemove', function(e) {
         let x = e.clientX / window.innerWidth;
-        let y = e.clientY / window.innerHeight;     
+        let y = e.clientY / window.innerHeight;
         first[i].style.transform = 'translate(-' + x * 15 + 'px, -' + y * 15 + 'px)';
-    });    
+    });
 }
 
 let second = document.querySelectorAll('.third');
 for (let i = 0; i < second.length; i++){
-    window.addEventListener('mousemove', function(e) { 
+    window.addEventListener('mousemove', function(e) {
         let x = e.clientX / window.innerWidth;
-        let y = e.clientY / window.innerHeight;     
+        let y = e.clientY / window.innerHeight;
         second[i].style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
-    });    
+    });
 }
 
-new PanelSnap({
-
-    // parent container
-    container: document.getElementById('projects'),
-
-    // panel selector
-    panelSelector: 'section',
-
-    // threshold
-    directionThreshold: 50,
-
-    // scroll delay
-    delay: 0,
-
-    // duration in milliseconds
-    duration: 300,
-
-    // easing function
-    easing: function(t) { return t }
+function sized() {
+    let size = window.innerWidth
+    if (size < 1000) {
+        new PanelSnap({
+        
+            // parent container
+        
+            container: document.removeAttribute('id'),
+            // panel selector
+            panelSelector: 'section',
+        
+            // threshold
+            directionThreshold: 50,
+        
+            // scroll delay
+            delay: 0,
+        
+            // duration in milliseconds
+            duration: 300,
+        
+            // easing function
+            easing: function(t) { return t }
+        
+        });
+    } else {
+        new PanelSnap({
+        
+            // parent container
     
-});
+            container: document.getElementById('projects'),
+            // panel selector
+            panelSelector: 'section',
+        
+            // threshold
+            directionThreshold: 50,
+        
+            // scroll delay
+            delay: 0,
+        
+            // duration in milliseconds
+            duration: 300,
+        
+            // easing function
+            easing: function(t) { return t }
+        
+        });
+    }
     
+}
+
+sized()
